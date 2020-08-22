7b7bab4fa8ed66e902eaacb3dfe71d5571fd3adb
Hridoy <hridoykrisnadas@gmail.com>
2020-08-22 22:39:18 +0600

    Initial commit

---
+++ app/src/main/java/com/moontechit/seoclerksnoads/MainActivity.java
@@ -0,0 +1,36 @@
+package com.moontechit.seoclerksnoads;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webViewID);
+        WebSettings webSettings = webView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://seoclerk.com");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+}
